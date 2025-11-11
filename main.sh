a27a767146ace0648fc71431e3e5cc506aa79885
Renan Brayner <renanbrayner@gmail.com>
2025-11-10 23:37:19 -0300

    feat: add ensure gum to main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 # Check if running as root
+ensure_gum() {
+    if ! command -v gum > /dev/null 2>&1; then
+        echo -n "The command gum was not found, do you wish to install it (Y/n)"
+	read -r answer
+	answer=${answer:-Y}
+
+	case "$answer" in
+            Y|y)
+                sudo pacman -S --noconfirm gum
+		;;
+            N|n)
+	        echo "gum is required. Exiting."
+		exit 1
+		;;
+	    *)
+	        echo "Invalid option. Exiting"
+		exit 1
+	        ;;
+	esac
+    fi
+}
+
+clear
+ensure_gum
+
 if [[ $EUID -eq 0 ]]; then
     echo "You are running this script as root."
     if ! gum confirm "Running as root may cause issues. Are you sure you to continue?"; then
